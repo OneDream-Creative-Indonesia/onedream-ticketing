8781c2c62e95b4fd2b36768500dbd945d6751e20
Your Name <dedenkoesuma72@gmail.com>
2024-06-14 21:47:37 +0700

    fix:bugs

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,10 @@
         <meta name="application-name" content="{{ config('app.name') }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{{ $title ?? config('app.name') }}</title>
+        <title> Photobooth By Onedream - One Dream Creative Indonesia</title>
+
+        <!-- ========== Favicon Icon ========== -->
+        <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
 
         <style>
             [x-cloak] {
