d19953444376ccc9805490f61cbe664143d97a21
Your Name <dedenkoesuma72@gmail.com>
2024-06-14 21:49:45 +0700

    fix:bugs

---
+++ app/Models/Ticketing.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticketing extends Model
+{
+    use HasFactory;
+     protected $fillable = [
+        'nama',
+        'kelas',
+        'telpon',
+        'transaction_type',
+     ];
+}
